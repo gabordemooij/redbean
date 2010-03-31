a9d65f2426954dc8b217624b08ddfce20eaa1c03
gabor de mooij <gabordemooij@gabor-de-mooijs-imac.local>
2010-03-31 10:56:06 +0200

    added readme

---
+++ RedBean/ExtAssociationManager.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * RedBean Extended Association
+ * @file			RedBean/ExtAssociationManager.php
+ * @description		Manages complex bean associations.
+ *					
+ * @author			Gabor de Mooij
+ * @license			BSD
+ *
+ * (c) G.J.G.T. (Gabor) de Mooij
+ * This source file is subject to the BSD license that is bundled
+ * with this source code in the file license.txt.
+ */
+class RedBean_ExtAssociationManager extends RedBean_AssociationManager {
+
+	/**
+	 * Associates two beans with eachother.
+	 * @param RedBean_OODBBean $bean1
+	 * @param RedBean_OODBBean $bean2
+	 */
+	public function extAssociate(RedBean_OODBBean $bean1, RedBean_OODBBean $bean2, RedBean_OODBBean $baseBean ) {
+		$table = $this->getTable( array($bean1->getMeta("type") , $bean2->getMeta("type")) );
+		$baseBean->setMeta("type", $table );
+		return $this->associateBeans( $bean1, $bean2, $baseBean );
+	}
+	
+	
+}
