128d951b319c8f77447aed4d3fdf79ab34c8cad1
Gabor de Mooij, Buurtnerd <buurtnerd@gmail.com>
2011-04-11 09:08:22 +0200

    fixed large number issue part 2

--- RedBean/Driver/PDO.php
+++ RedBean/Driver/PDO.php
@@ -363,12 +363,12 @@ class RedBean_Driver_PDO implements RedBean_Driver {
 				foreach($aValues as $key=>&$value) {
 
 					if (is_integer($key)) {
-						if (ctype_digit(strval($value))) $s->bindParam($key+1,$value,PDO::PARAM_INT);
+						if (ctype_digit(strval($value)) && $value < 2147483648) { $s->bindParam($key+1,$value,PDO::PARAM_INT); }
 						else $s->bindParam($key+1,$value,PDO::PARAM_STR);
 					}
 					else {
 
-						if (ctype_digit(strval($value))) $s->bindParam($key,$value,PDO::PARAM_INT);
+						if (ctype_digit(strval($value)) &&  $value < 2147483648) $s->bindParam($key,$value,PDO::PARAM_INT);
 						else $s->bindParam($key,$value,PDO::PARAM_STR);
 					}
 
