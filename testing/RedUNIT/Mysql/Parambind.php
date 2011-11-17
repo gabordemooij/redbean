5aefa3b0a851e94376c8f34c826878e5489db1a5
Gabor de Mooij <gabordemooij@gmail.com>
2011-11-17 18:10:18 +0100

    big test cleanup

--- testing/RedUNIT/Mysql/Parambind.php
+++ testing/RedUNIT/Mysql/Parambind.php
@@ -4,6 +4,12 @@ class RedUNIT_Mysql_Parambind extends RedUNIT_Mysql {
 
 	public function run() {
 				
+		$toolbox = R::$toolbox;
+		$adapter = $toolbox->getDatabaseAdapter();
+		$writer  = $toolbox->getWriter();
+		$redbean = $toolbox->getRedBean();
+		$pdo = $adapter->getDatabase();
+		
 		R::$adapter->getDatabase()->flagUseStringOnlyBinding = TRUE;
 		try{R::getAll("select * from job limit ? ", array(1)); fail(); }catch(Exception $e){ pass(); }
 		try{R::getAll("select * from job limit :l ", array(":l"=>1)); fail(); }catch(Exception $e){ pass(); }
@@ -22,7 +28,24 @@ class RedUNIT_Mysql_Parambind extends RedUNIT_Mysql {
 		asrt((count($person)>0), true);
 		$musician = array_pop($person);
 		asrt(intval($musician->id),0);
-				
+		
+		try {
+			$adapter->exec("an invalid query");
+			fail();
+		}catch(RedBean_Exception_SQL $e ) {
+			pass();
+		}
+		asrt( (int) $adapter->getCell("SELECT 123") ,123);
+		asrt( (int) $adapter->getCell("SELECT ?",array("987")) ,987);
+		asrt( (int) $adapter->getCell("SELECT ?+?",array("987","2")) ,989);
+		asrt( (int) $adapter->getCell("SELECT :numberOne+:numberTwo",array(
+				  ":numberOne"=>42,":numberTwo"=>50)) ,92);
+		$pair = $adapter->getAssoc("SELECT 'thekey','thevalue' ");
+		asrt(is_array($pair),true);
+		asrt(count($pair),1);
+		asrt(isset($pair["thekey"]),true);
+		asrt($pair["thekey"],"thevalue");
+					
 	}
 
 }
