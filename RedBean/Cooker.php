e0772671dc4042534efc2fd4c4e336ada291dd29
Andrey Murashkin <murich.ua@gmail.com>
2011-12-31 10:12:05 +0200

    allows to send multiple form values

--- RedBean/Cooker.php
+++ RedBean/Cooker.php
@@ -65,6 +65,21 @@ class RedBean_Cooker {
 	 * @return	array $beans beans
 	 */
 	public function graph( $array, $filterEmpty = false ) {
+                if(is_array($array) && isset($array['type']) && is_array(@$array['id'])){ // allowing multi-select and multi-checkbox by restructuring the array.
+                    foreach($array['id'] as $key=>$value){
+                        foreach($array as $kk=>$vv){
+                            if(is_array($vv)) {
+                                if(isset($vv[$key])) $new[$key][$kk] = $vv[$key];
+                            }
+                            else $new[$key][$kk] = $vv;
+                        }
+                        
+                    }
+                    $array = $new;
+                    unset($new);
+                    print_r($array);
+                }
+                
 		$beans = array();
 		if (is_array($array) && isset($array['type'])) {
 			$type = $array['type'];
