424f98b68077b78ef0ad62cd38e5bce39dee570c
Gabor de Mooij <gabordemooij@gmail.com>
2012-07-28 13:55:02 +0200

    Added support for rolling different flavours with replica.

--- replica.php
+++ replica.php
@@ -1,24 +1,39 @@
 <?php
 $pat = "/\/\*([\n]|.)+?\*\//";
 
-$code = "";
+$code = array('default'=>"");
 $loader = simplexml_load_file("replica.xml");
 $items = $loader->load->item;
 foreach($items as $item) {
-    echo "Adding: $item \n";
-    $code .= file_get_contents( $item ) . "\n";
+    $a = $item->attributes();
+    if ($a && isset($a->flavour)) { 
+	$fl = $a->flavour->__toString();  
+	if (!isset($code[$fl])) $code[$fl] = $code['default'];
+	$code[$fl] .= file_get_contents( $item ) . "\n";
+	echo "Adding: $item to flavour: $fl \n";
+    }
+    else {	
+    	echo "Adding: $item \n";
+    	$c = file_get_contents( $item ) . "\n";
+	foreach($code as $k=>$codeFlav) $code[$k] .= $c;
+   }
 }
-$code .= "
+foreach($code as $f=>$codeFlav) {
+$code[$f] .= "
 
 class R extends RedBean_Facade{
 }
 ";
 
+
+
 //Clean php tags and whitespace from codebase.
-$code = "<?php ".str_replace( array("<?php", "<?", "?>"), array("", "", ""), $code);
-file_put_contents("rb.php", $code);
+$code[$f] = "<?php ".str_replace( array("<?php", "<?", "?>"), array("", "", ""), $code[$f]);
+file_put_contents("rb".($f==='default' ? '' : $f).".php", $code[$f]);
 
 if ($_SERVER['argc']>1 && $_SERVER['argv'][1]=='-s')
-file_put_contents("rb.php", php_strip_whitespace("rb.php"));
+file_put_contents("rb".($f==='default' ? '' : $f).".php", 
+	php_strip_whitespace("rb".($f==='default' ? '' : $f).".php"));
 
+}
 ?>
