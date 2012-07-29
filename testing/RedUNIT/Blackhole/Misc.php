6fcfc93a35c1faa5f669e6abe42559824b4834bc
Gabor de Mooij <gabordemooij@gmail.com>
2012-07-29 14:44:57 +0200

    Make tests run easier for OCI testing.

--- testing/RedUNIT/Blackhole/Misc.php
+++ testing/RedUNIT/Blackhole/Misc.php
@@ -14,6 +14,14 @@
  */
 
 class RedUNIT_Blackhole_Misc extends RedUNIT_Blackhole {
+	
+	/*
+	 * What drivers should be loaded for this test pack? 
+	 */
+	public function getTargetDrivers() {
+		return array('sqlite');
+	}
+	
 	/**
 	 * Begin testing.
 	 * This method runs the actual test pack.
