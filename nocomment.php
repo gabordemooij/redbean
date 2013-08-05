d676b461751b08ee262ceaed5bc3c42824c57e6c
David Deutsch <skore@valanx.org>
2013-08-05 21:57:22 +0200

    might as well clean these up, too

--- nocomment.php
+++ nocomment.php
@@ -1,36 +1,35 @@
 <?php
-$fileStr = file_get_contents('rb.php');
+
+$fileStr = file_get_contents( 'rb.php' );
+
 $newStr  = '';
 
-
-function removeEmptyLines($string)
+function removeEmptyLines( $string )
 {
-return preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $string);
+	return preg_replace( "/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $string );
 }
 
-$commentTokens = array(T_COMMENT);
+$commentTokens = array( T_COMMENT );
 
-if (defined('T_DOC_COMMENT'))
-    $commentTokens[] = T_DOC_COMMENT; // PHP 5
-if (defined('T_ML_COMMENT'))
-    $commentTokens[] = T_ML_COMMENT;  // PHP 4
+if ( defined( 'T_DOC_COMMENT' ) ) $commentTokens[] = T_DOC_COMMENT; // PHP 5
 
-$tokens = token_get_all($fileStr);
+if ( defined( 'T_ML_COMMENT' ) ) $commentTokens[] = T_ML_COMMENT; // PHP 4
 
-foreach ($tokens as $token) {    
-    if (is_array($token)) {
-        if (in_array($token[0], $commentTokens))
-            continue;
+$tokens = token_get_all( $fileStr );
 
-        $token = $token[1];
-    }
+foreach ( $tokens as $token ) {
+	if ( is_array( $token ) ) {
+		if ( in_array( $token[0], $commentTokens ) ) continue;
 
-    $newStr .= $token;
+		$token = $token[1];
+	}
+
+	$newStr .= $token;
 }
 
-$newStr = removeEmptyLines($newStr);
+$newStr = removeEmptyLines( $newStr );
 
-$newStr = str_replace("<"."?php","",$newStr);
-$newStr = "<"."?php\n//Written by Gabor de Mooij and the RedBeanPHP Community, copyright 2009-2012\n//Licensed New BSD/GPLV2 - see license.txt\n".$newStr;
+$newStr = str_replace( "<" . "?php", "", $newStr );
+$newStr = "<" . "?php\n//Written by Gabor de Mooij and the RedBeanPHP Community, copyright 2009-2012\n//Licensed New BSD/GPLV2 - see license.txt\n" . $newStr;
 
-file_put_contents('rbnc.php',$newStr);
+file_put_contents( 'rbnc.php', $newStr );
