8b3485c157160cf5e99dae8c3498488073a76f57
Gabor de Mooij <gabordemooij@gmail.com>
2013-09-27 23:57:26 +0200

    Improve docs logger.

--- RedBean/Logger.php
+++ RedBean/Logger.php
@@ -1,14 +1,13 @@
 <?php
 /**
- * RedBean Logging
+ * RedBean Logging interface.
+ * Provides a uniform and convenient logging
+ * interface throughout RedBeanPHP.
  *
  * @file    RedBean/Logging.php
  * @desc    Logging interface for RedBeanPHP ORM
  * @author  Gabor de Mooij and the RedBeanPHP Community
  * @license BSD/GPLv2
- *
- * Provides a uniform and convenient logging
- * interface throughout RedBeanPHP.
  *
  * copyright (c) G.J.G.T. (Gabor) de Mooij and the RedBeanPHP Community
  * This source file is subject to the BSD/GPLv2 License that is bundled
@@ -18,10 +17,14 @@ interface RedBean_Logger
 {
 
 	/**
-	 * Logs a message specified in first argument.
-	 *
-	 * @param string $message the message to log. (optional)
-	 *
+	 * A logger (for PDO or OCI driver) needs to implement the log method.
+	 * The log method will receive logging data. Note that the number of parameters is 0, this means
+	 * all parameters are optional and the number may vary. This way the logger can be used in a very
+	 * flexible way. Sometimes the logger is used to log a simple error message and in other
+	 * situation sql and bindings are passed.
+	 * The log method should be able to accept all kinds of parameters and data by using
+	 * functions like func_num_args/func_get_args.
+	 * 
 	 * @return void
 	 */
 	public function log();
