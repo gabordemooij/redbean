70a179ceac40645c93a8b9e89707e609e6e21754
Gabor de Mooij <gabordemooij@gmail.com>
2013-11-14 22:44:37 +0100

    Cleanup.

--- replica2.php
+++ replica2.php
@@ -4,15 +4,7 @@
 @mkdir('build');
 @exec('cp -R RedBeanPHP build/RedBeanPHP');
 @exec('mv build/RedBeanPHP/loader.php build/');
-  
-$phar = new Phar("rb.phar", 
-    //FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME,
-	 0, "rb.phar");
-//$phar["index.php"] = file_get_contents($srcRoot . "/index.php");
-//$phar["common.php"] = file_get_contents($srcRoot . "/common.php");
-
+$phar = new Phar("rb.phar", 0, "rb.phar");
 $phar->buildFromDirectory('./build');
-
-
 $phar->setStub($phar->createDefaultStub("loader.php"));
  
