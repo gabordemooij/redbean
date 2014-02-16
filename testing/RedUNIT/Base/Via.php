f9e187255d1886e2589194377846dbfc758de087
Gabor de Mooij <gabordemooij@gmail.com>
2014-02-16 12:06:55 +0100

    More tests.

--- testing/RedUNIT/Base/Via.php
+++ testing/RedUNIT/Base/Via.php
@@ -67,4 +67,166 @@ class Via extends Base
 		asrt(count($coders), 1);
 		asrt($john->name, 'John');
 	}
+	
+	/**
+	 * Test Via and Link together.
+	 * 
+	 * @return void
+	 */
+	public function testViaAndLink()
+	{
+		R::nuke();
+		list( $John, $Anna, $Celine ) = R::dispense( 'employee', 3 );
+		$John->badge   = 'John';
+		$Anna->badge   = 'Anna';
+		$Celine->badge = 'Celine';
+
+		$project = R::dispense( 'project' );
+		$project->name = 'x';
+
+		$project2 = R::dispense( 'project' );
+		$project2->name = 'y';
+
+		$John->link( 'participant', array(
+			 'arole' => 'designer'
+		) )->project = $project;
+
+		$Anna->link( 'participant', array(
+			 'arole' => 'developer'
+		) )->project = $project;
+
+		$Celine->link( 'participant', array(
+			 'arole' => 'sales'
+		) )->project = $project2;
+
+		$Anna->link('participant', array(
+			 'arole' => 'lead'
+		) )->project = $project2;
+
+		R::storeAll( array( $project, $project2, $John, $Anna, $Celine )  );
+
+		$employees = $project
+			->with(' ORDER BY badge ASC ')
+			->via( 'participant' )
+			->sharedEmployee;
+
+		asrt( is_array( $employees ), TRUE );
+		asrt( count( $employees ), 2 );
+
+		$badges = array();
+		foreach( $employees as $employee ) {
+			$badges[] = $employee->badge;
+		}
+
+		asrt( implode( ',', $badges ), 'Anna,John' );
+
+		$employees = $project2
+			->with(' ORDER BY badge ASC ')
+			->via( 'participant' )
+			->sharedEmployee;
+
+		asrt( is_array( $employees ), TRUE );
+		asrt( count( $employees ), 2 );
+
+		$badges = array();
+		foreach( $employees as $employee ) {
+			$badges[] = $employee->badge;
+		}
+
+		asrt( implode( ',', $badges ), 'Anna,Celine' );
+
+		$projects = $John->sharedProject;
+
+		asrt( is_array( $projects ), TRUE );
+		asrt( count( $projects ), 1 );
+
+		$projectList = array();
+		foreach( $projects as $project ) {
+			$projectList[] =  $project->name;
+		}
+
+		sort( $projectList );
+		asrt( implode( ',', $projectList ), 'x' );
+
+		$projects = $Anna->sharedProject;
+
+		asrt( is_array( $projects ), TRUE );
+		asrt( count( $projects ), 2 );
+
+		$projectList = array();
+		foreach( $projects as $project ) {
+			$projectList[] =  $project->name;
+		}
+
+		sort( $projectList );
+		asrt( implode( ',', $projectList ), 'x,y' );
+
+		$projects = $Anna->via( 'participant' )->sharedProject;
+
+		asrt( is_array( $projects ), TRUE );
+		asrt( count( $projects ), 2 );
+
+		$projectList = array();
+		foreach( $projects as $project ) {
+			$projectList[] =  $project->name;
+		}
+
+		sort( $projectList );
+		asrt( implode( ',', $projectList ), 'x,y' );
+
+		$projects = $Celine->via( 'participant' )->sharedProject;
+
+		asrt( is_array( $projects ), TRUE );
+		asrt( count( $projects ), 1 );
+
+		$projectList = array();
+		foreach( $projects as $project ) {
+			$projectList[] =  $project->name;
+		}
+
+		sort( $projectList );
+		asrt( implode( ',', $projectList ), 'y' );
+
+		$roles = $Anna->ownParticipant;
+
+		asrt( is_array( $roles ), TRUE );
+		asrt( count( $roles ), 2 );
+
+		$roleList = array();
+		foreach( $roles as $role ) {
+			$roleList[] = $role->arole;
+		}
+
+		sort( $roleList );
+		asrt( implode( ',', $roleList ), 'developer,lead' );
+
+		$project2->sharedEmployee[] = $John;
+		R::store( $project2 );
+
+		$projects = $John->sharedProject;
+
+		asrt( is_array( $projects ), TRUE );
+		asrt( count( $projects ), 2 );
+
+		$projectList = array();
+		foreach( $projects as $project ) {
+			$projectList[] =  $project->name;
+		}
+
+		sort( $projectList );
+		asrt( implode( ',', $projectList ), 'x,y' );
+
+		$projects = $John->via( 'participant' )->sharedProject;
+
+		asrt( is_array( $projects ), TRUE );
+		asrt( count( $projects ), 2 );
+
+		$projectList = array();
+		foreach( $projects as $project ) {
+			$projectList[] =  $project->name;
+		}
+
+		sort( $projectList );
+		asrt( implode( ',', $projectList ), 'x,y' );
+	}
 }
