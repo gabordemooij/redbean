065b3dea34c60b6e71d5f89f5430981a42960f94
Gabor de Mooij <gabordemooij@gmail.com>
2014-03-20 23:15:02 +0100

    Added test hook example.

---
+++ testing/cli/test_hook_example.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * 
+ * Here you can add logic to be executed during the bootstrapping
+ * of RedUNIT.
+ * 
+ * You can define/use the following variables:
+ * 
+ * $hookPath -> add 1 additional path which will be searched for unit tests
+ * $extraTestsFromHook -> array with additional test packs (will be executed if you pass 'all' argument to CLI runner)
+ * 
+ * 
+ */
