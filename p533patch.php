03ed0dec037e05f7836ac6631f8e30f8b37c1ce9
Gabor de Mooij <gabordemooij@gmail.com>
2014-05-26 23:24:01 +0200

    P533 patch

---
+++ p533patch.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Running Patch P533...";
+echo PHP_EOL;
+
+$code = file_get_contents('rb.php');
+$code = str_replace('&__offsetGet', '__offsetGet', $code);
+
+$bytes = file_put_contents('rb-p533.php', $code);
+
+if ($bytes > 0) {
+	echo 'Applied patch for PHP < 5.3.3';
+	echo PHP_EOL;
+	exit;
+} else {
+	echo 'Somthing went wrong.';
+	echo PHP_EOL;
+	exit;
+}
