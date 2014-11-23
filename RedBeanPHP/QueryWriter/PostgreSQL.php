dd07d9bccca9899f8c25bde7321c6508b5b2c54f
Gabor de Mooij <gabordemooij@gmail.com>
2014-11-23 12:16:52 +0100

    Support more currency symbols, closes #398.

--- RedBeanPHP/QueryWriter/PostgreSQL.php
+++ RedBeanPHP/QueryWriter/PostgreSQL.php
@@ -225,7 +225,7 @@ class PostgreSQL extends AQueryWriter implements QueryWriter
 				return PostgreSQL::C_DATATYPE_SPECIAL_POLYGON;
 			}
 
-			if ( preg_match( '/^\-?\$[\d,\.]+$/', $value ) ) {
+			if ( preg_match( '/^\-?(\$|€|¥|£)[\d,\.]+$/', $value ) ) {
 				return PostgreSQL::C_DATATYPE_SPECIAL_MONEY;
 			}
 		}
