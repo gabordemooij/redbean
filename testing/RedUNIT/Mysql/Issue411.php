6e433473b2b0a2e6acd90f87dcf981f5665d896a
Gabor de Mooij <gabor@redbeanphp.com>
2015-01-02 12:14:44 +0000

    Added unit test for issue411, close #411.

---
+++ testing/RedUNIT/Mysql/Issue411.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace RedUNIT\Mysql;
+
+use RedUNIT\Mysql as Mysql;
+use RedBeanPHP\Facade as R;
+
+/**
+ * Issue 411
+ *
+ * @file    RedUNIT/Mysql/Issue411.php
+ * @desc    Tests intermediate varchar 191 type for MySQL utf8mb4.
+ * @author  Gabor de Mooij and the RedBeanPHP Community
+ * @license New BSD/GPLv2
+ *
+ * (c) G.J.G.T. (Gabor) de Mooij and the RedBeanPHP Community.
+ * This source file is subject to the New BSD/GPLv2 License that is bundled
+ * with this source code in the file license.txt.
+ */
+class Issue411 extends Mysql
+{
+
+	/**
+	 * Test varchar 191 condition.
+	 *
+	 * @return void
+	 */
+	public function testInnoDBIndexLimit()
+	{
+		R::nuke();
+		$book = R::dispense( 'book' );
+		$book->text = 'abcd';
+		R::store( $book );
+		$columns = R::inspect( 'book' );
+		asrt( isset( $columns['text'] ), TRUE );
+		asrt( $columns['text'], 'varchar(191)' );
+		$book = $book->fresh();
+		$book->text = str_repeat( 'x', 190 );
+		R::store( $book );
+		$columns = R::inspect( 'book' );
+		asrt( isset( $columns['text'] ), TRUE );
+		asrt( $columns['text'], 'varchar(191)' );
+		$book = $book->fresh();
+		$book->text = str_repeat( 'x', 191 );
+		R::store( $book );
+		$columns = R::inspect( 'book' );
+		asrt( isset( $columns['text'] ), TRUE );
+		asrt( $columns['text'], 'varchar(191)' );
+		$book = $book->fresh();
+		$book->text = str_repeat( 'x', 192 );
+		R::store( $book );
+		$columns = R::inspect( 'book' );
+		asrt( isset( $columns['text'] ), TRUE );
+		asrt( $columns['text'], 'varchar(255)' );
+	}
+}
+
+
+
+
