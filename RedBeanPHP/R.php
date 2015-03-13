d8f9f4f5ab01b285a587dd54f5175d013c316ade
Gabor de Mooij <gabor@redbeanphp.com>
2015-03-13 08:21:51 +0100

    Added documentation to R class.

--- RedBeanPHP/R.php
+++ RedBeanPHP/R.php
@@ -2,4 +2,18 @@
 
 namespace RedBeanPHP;
 
-class R extends Facade {}
+/**
+ * R-Facade (for Composer)
+ *
+ * If you use Composer you don't use the rb.php file which
+ * has the R-facade, so here is a separate, namespaced R-facade for
+ * those that prefer this.
+ *
+ * An alternative option might be to alias RedBeanPHP/Facade.
+ *
+ * @file    RedBeanPHP/R.php
+ * @author  Simirimia
+ * @license BSD/GPLv2
+ *
+ */
+ class R extends Facade {}
