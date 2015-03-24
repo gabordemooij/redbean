d6386b9a12f1094ca1298be8c8a49c5961c83767
Gabor de Mooij <gabor@redbeanphp.com>
2015-03-24 18:22:04 +0100

    Make sure empty records from joins do not produce unnecessary beans.

--- RedBeanPHP/Finder.php
+++ RedBeanPHP/Finder.php
@@ -351,6 +351,7 @@ class Finder
 						$wannaBean[$property] = $value;
 					}
 				}
+				if ( is_null( $wannaBean['id'] ) ) continue;
 				$wannaBeans[$type][$wannaBean['id']] = $wannaBean;
 			}
 		}
