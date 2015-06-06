b48b3400e740173d43f06015b37fe792a8d6ac7d
Gabor de Mooij <gabor@redbeanphp.com>
2015-06-06 11:06:17 +0200

    Performance improvement for MySQL servers with lots of databases in fluid mode.

--- RedBeanPHP/QueryWriter/MySQL.php
+++ RedBeanPHP/QueryWriter/MySQL.php
@@ -57,6 +57,7 @@ class MySQL extends AQueryWriter implements QueryWriter
 	 */
 	protected function getKeyMapForType( $type )
 	{
+		$databaseName = $this->adapter->getCell('SELECT DATABASE()');
 		$table = $this->esc( $type, TRUE );
 		$keys = $this->adapter->get('
 			SELECT
@@ -68,17 +69,15 @@ class MySQL extends AQueryWriter implements QueryWriter
 				information_schema.referential_constraints.delete_rule AS `on_delete`
 				FROM information_schema.key_column_usage
 				INNER JOIN information_schema.referential_constraints
-					ON (
-						information_schema.referential_constraints.constraint_name = information_schema.key_column_usage.constraint_name
-						AND information_schema.referential_constraints.constraint_schema = information_schema.key_column_usage.constraint_schema
-						AND information_schema.referential_constraints.constraint_catalog = information_schema.key_column_usage.constraint_catalog
-					)
+				ON information_schema.referential_constraints.constraint_name = information_schema.key_column_usage.constraint_name
 			WHERE
-				information_schema.key_column_usage.table_schema IN ( SELECT DATABASE() )
-				AND information_schema.key_column_usage.table_name = ?
+				information_schema.key_column_usage.table_schema = :database
+				AND information_schema.referential_constraints.constraint_schema  = :database
+				AND information_schema.key_column_usage.constraint_schema  = :database
+				AND information_schema.key_column_usage.table_name = :table
 				AND information_schema.key_column_usage.constraint_name != \'PRIMARY\'
 				AND information_schema.key_column_usage.referenced_table_name IS NOT NULL
-		', array($table));
+		', array( ':database' => $databaseName, ':table' => $table ) );
 		$keyInfoList = array();
 		foreach ( $keys as $k ) {
 			$label = $this->makeFKLabel( $k['from'], $k['table'], $k['to'] );
