8ac89f31bb4b4e16ec84c36c3de307acb7420e5a
Gabor de Mooij <mail@gabordemooij.com>
2016-04-25 22:58:25 +0200

    Improve docs to RedUNIT.

--- testing/RedUNIT.php
+++ testing/RedUNIT.php
@@ -30,45 +30,48 @@ abstract class RedUNIT
 	protected $currentlyActiveDriverID = 'unknown';
 
 	/**
-	 * What drivers should be loaded for this test pack?
+	 * What drivers should be loaded for this test pack ?
+	 * This method should be implemented by the test to tell the
+	 * test controller system what drivers are supposed to be tested.
+	 * Each driver will be fed to the test in a 'round' (explained below).
+	 *
+	 * @return array
 	 */
 	abstract public function getTargetDrivers();
 
 	/**
-	 * Prepare test pack (mostly: nuke the entire database)
+	 * Prepare test pack (mostly: nuke the entire database).
+	 * This method prepares the test for a run.
 	 */
 	public function prepare()
 	{
 		R::freeze( FALSE );
-
 		R::debug( FALSE );
-
 		R::nuke();
 	}
 
 	/**
-	 * Run the actual test
+	 * Runs the test. This method will run the tests implemented
+	 * by the RedUNIT instance. The run method scans its class for
+	 * all public instance methods except:
+	 * run (to avoid recursion), getTargetDrivers, onEvent
+	 * and prepare. -- added cleanUp/prepare just in case they get overridden.
+	 *
+	 * @return void
 	 */
 	public function run()
 	{
 		$class = new \ReflectionClass( $this );
-
-		$skip = array( 'run', 'getTargetDrivers', 'onEvent');
+		$skip = array( 'run', 'getTargetDrivers', 'onEvent', 'cleanUp', 'prepare' );
 		// Call all methods except run automatically
 		foreach ( $class->getMethods( \ReflectionMethod::IS_PUBLIC ) as $method ) {
 			// Skip methods inherited from parent class
 			if ( $method->class != $class->getName() ) continue;
-
 			if ( in_array( $method->name, $skip ) ) continue;
-
 			$classname = str_replace( $class->getParentClass()->getName().'_', '', $method->class );
-
 			printtext( "\n\t" . $classname."->".$method->name." [".$method->class."->".$method->name."]" . " \n\t" );
-
 			$call = $method->name;
-
 			$this->$call();
-
 			try {
 				R::nuke();
 			} catch( \PDOException $e ) {
@@ -78,7 +81,11 @@ abstract class RedUNIT
 	}
 
 	/**
-	 * Do some cleanup (if necessary..)
+	 * Clean-up method, to be invoked after running the test.
+	 * This is an empty implementation, it does nothing. However this method
+	 * should be overridden by tests that require clean-up.
+	 *
+	 * @return void
 	 */
 	public function cleanUp()
 	{
@@ -86,6 +93,15 @@ abstract class RedUNIT
 
 	/**
 	 * Sets the current driver.
+	 * This method is called by a test controller, runner or manager
+	 * to activate the mode associated with the specified driver
+	 * identifier. This mechanism allows a test to run slightly different
+	 * in the context of another driver, for instance SQLite might not need
+	 * some tests, or MySQL might need some additional tests etc...
+	 *
+	 * @param string $driver the driver identifier
+	 *
+	 * @return void
 	 */
 	public function setCurrentDriver( $driver )
 	{
@@ -94,8 +110,15 @@ abstract class RedUNIT
 
 	/**
 	 * Sets the round number.
+	 * Each test can have a varying number of flavors.
+	 * A test flavor is 'that same test' but for a different driver.
+	 * Each 'run on a specific driver' is called a round.
+	 * Some tests might want to know what the current round is.
+	 * This method can be used to set the current round number.
 	 *
-	 * @param integer $roundNumber round
+	 * @param integer $roundNumber round, the current round number
+	 *
+	 * @return void
 	 */
 	public function setRound( $roundNumber )
 	{
@@ -103,7 +126,9 @@ abstract class RedUNIT
 	}
 
 	/**
-	 * Returns the current round number
+	 * Returns the current round number.
+	 * The current round number indicates how many times
+	 * this test has been applied (to various drivers).
 	 *
 	 * @return integer
 	 */
@@ -113,7 +138,11 @@ abstract class RedUNIT
 	}
 
 	/**
-	 * Returns TRUE if the current round is the first round.
+	 * Detects whether the current round is the first one.
+	 * If the current round is indeed the first round, this method
+	 * will return boolean TRUE, otherwise it will return FALSE. Note that
+	 * the rounds are 0-based, so - if the current round equals 0, this
+	 * method will return TRUE.
 	 *
 	 * @return boolean
 	 */
