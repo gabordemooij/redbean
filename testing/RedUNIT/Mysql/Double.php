3cb5040e35acf5579afeaf841fc27e30a134e5ef
Gabor de Mooij <mail@gabordemooij.com>
2016-05-04 08:43:07 +0200

    Explain use of Double test and clean up that test file.

--- testing/RedUNIT/Mysql/Double.php
+++ testing/RedUNIT/Mysql/Double.php
@@ -8,6 +8,9 @@ use RedBeanPHP\Facade as R;
 /**
  * Double
  *
+ * Tests whether double precision values are correctly stored and
+ * preserved.
+ *
  * @file    RedUNIT/Mysql/Double.php
  * @desc    Tests handling of double precision values.
  * @author  Gabor de Mooij and the RedBeanPHP Community
@@ -31,29 +34,17 @@ class Double extends Mysql
 		$writer  = $toolbox->getWriter();
 		$redbean = $toolbox->getRedBean();
 		$pdo     = $adapter->getDatabase();
-
 		$largeDouble = 999999888889999922211111; //8.88889999922211e+17;
-
 		$page = $redbean->dispense( "page" );
-
 		$page->weight = $largeDouble;
-
 		$id = $redbean->store( $page );
-
-		$cols = $writer->getColumns( "page" );
-
-		asrt( $cols["weight"], "double" );
-
-		$page = $redbean->load( "page", $id );
-
-		$page->name = "dont change the numbers!";
-
+		$cols = $writer->getColumns( 'page' );
+		asrt( $cols['weight'], 'double' );
+		$page = $redbean->load( 'page', $id );
+		$page->name = 'dont change the numbers!';
 		$redbean->store( $page );
-
-		$page = $redbean->load( "page", $id );
-
-		$cols = $writer->getColumns( "page" );
-
-		asrt( $cols["weight"], "double" );
+		$page = $redbean->load( 'page', $id );
+		$cols = $writer->getColumns( 'page' );
+		asrt( $cols['weight'], 'double' );
 	}
 }
