95f1122c8e3714413480868eddcf2e06b0f77107
Gabor de Mooij <mail@gabordemooij.com>
2016-05-06 00:37:50 +0200

    Remove Mix Test.

--- testing/RedUNIT/Mysql/Preexist.php
+++ testing/RedUNIT/Mysql/Preexist.php
@@ -9,6 +9,9 @@ use RedBeanPHP\AssociationManager as AssociationManager;
 /**
  * Preexist
  *
+ * Tests whether RedBeanPHP can work with existing
+ * MySQL schemas.
+ *
  * @file    RedUNIT/Mysql/Preexist.php
  * @desc    Tests integration with pre-existing schemas.
  * @author  Gabor de Mooij and the RedBeanPHP Community
@@ -32,43 +35,24 @@ class Preexist extends Mysql
 		$writer      = $toolbox->getWriter();
 		$redbean     = $toolbox->getRedBean();
 		$pdo         = $adapter->getDatabase();
-
 		$a           = new AssociationManager( $toolbox );
-
 		$page        = $redbean->dispense( "page" );
-
 		$page->name  = "John's page";
-
 		$idpage      = $redbean->store( $page );
-
 		$page2       = $redbean->dispense( "page" );
-
 		$page2->name = "John's second page";
-
 		$idpage2     = $redbean->store( $page2 );
-
 		$a->associate( $page, $page2 );
-
 		$adapter->exec( "ALTER TABLE " . $writer->esc( 'page' ) . "
 		CHANGE " . $writer->esc( 'name' ) . " " . $writer->esc( 'name' ) . "
 		VARCHAR( 254 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL " );
-
 		$page       = $redbean->dispense( "page" );
-
 		$page->name = "Just Another Page In a Table";
-
 		$cols       = $writer->getColumns( "page" );
-
 		asrt( $cols["name"], "varchar(254)" );
-
-		//$pdo->SethMode(1);
-
 		$redbean->store( $page );
-
 		pass(); // No crash?
-
 		$cols = $writer->getColumns( "page" );
-
 		asrt( $cols["name"], "varchar(254)" ); //must still be same
 	}
 }
