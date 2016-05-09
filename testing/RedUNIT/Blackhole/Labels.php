932522ead368ef3849e643e6c7faab1e001f50ae
Gabor de Mooij <mail@gabordemooij.com>
2016-05-09 09:01:06 +0200

    Explain the purpose of the Labels test suite.

--- testing/RedUNIT/Blackhole/Labels.php
+++ testing/RedUNIT/Blackhole/Labels.php
@@ -9,6 +9,12 @@ use RedBeanPHP\OODBBean as OODBBean;
 /**
  * Labels
  *
+ * Labels are very simple beans, only having a title and an ID.
+ * The idea behind labels is that they are very easy to generate
+ * from a comma separated list and they can easily be converted
+ * to such a list. Labels can be used for simplistic relations in the
+ * database like tagging or enums.
+ *
  * @file    RedUNIT/Blackhole/Labels.php
  * @desc    Tests Facade Label functions.
  * @author  Gabor de Mooij and the RedBeanPHP Community
@@ -29,19 +35,13 @@ class Labels extends Blackhole
 	public function testLabels()
 	{
 		testpack( 'Test Labels' );
-
 		$meals = R::dispenseLabels( 'meal', array( 'meat', 'fish', 'vegetarian' ) );
-
 		asrt( is_array( $meals ), TRUE );
-
 		asrt( count( $meals ), 3 );
-
 		foreach ( $meals as $m ) {
 			asrt( ( $m instanceof OODBBean ), TRUE );
 		}
-
 		$listOfMeals = implode( ',', R::gatherLabels( $meals ) );
-
 		asrt( $listOfMeals, 'fish,meat,vegetarian' );
 	}
 }
