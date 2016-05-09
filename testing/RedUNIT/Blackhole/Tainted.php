751338b0a2cccb1fe0d915a54f8a653c13ae1fe0
Gabor de Mooij <mail@gabordemooij.com>
2016-05-09 18:53:56 +0200

    Explain Tainted tests.

--- testing/RedUNIT/Blackhole/Tainted.php
+++ testing/RedUNIT/Blackhole/Tainted.php
@@ -8,6 +8,9 @@ use RedBeanPHP\Facade as R;
 /**
  * Tainted
  *
+ * This test suite tests whether beans are marked as tainted
+ * under the correct circumstances.
+ *
  * @file    RedUNIT/Blackhole/Tainted.php
  * @desc    Tests tainted flag for OODBBean objects.
  * @author  Gabor de Mooij and the RedBeanPHP Community
@@ -93,66 +96,36 @@ class Tainted extends Blackhole
 	public function testTainted()
 	{
 		testpack( 'Original Tainted Tests' );
-
 		$redbean = R::getRedBean();
-
 		$spoon = $redbean->dispense( "spoon" );
-
 		asrt( $spoon->getMeta( "tainted" ), TRUE );
-
 		$spoon->dirty = "yes";
-
 		asrt( $spoon->getMeta( "tainted" ), TRUE );
-
 		testpack( 'Tainted List test' );
-
 		$note = R::dispense( 'note' );
-
 		$note->text = 'abc';
-
 		$note->ownNote[] = R::dispense( 'note' )->setAttr( 'text', 'def' );
-
 		$id = R::store( $note );
-
 		$note = R::load( 'note', $id );
-
 		asrt( $note->isTainted(), FALSE );
-
 		// Shouldn't affect tainted
 		$note->text;
-
 		asrt( $note->isTainted(), FALSE );
-
 		$note->ownNote;
-
 		asrt( $note->isTainted(), TRUE );
-
 		testpack( 'Tainted Test Old Value' );
-
 		$text = $note->old( 'text' );
-
 		asrt( $text, 'abc' );
-
 		asrt( $note->hasChanged( 'text' ), FALSE );
-
 		$note->text = 'xxx';
-
 		asrt( $note->hasChanged( 'text' ), TRUE );
-
 		$text = $note->old( 'text' );
-
 		asrt( $text, 'abc' );
-
 		testpack( 'Tainted Non-exist' );
-
 		asrt( $note->hasChanged( 'text2' ), FALSE );
-
 		testpack( 'Misc Tainted Tests' );
-
 		$bean = R::dispense( 'bean' );
-
 		$bean->hasChanged( 'prop' );
-
 		$bean->old( 'prop' );
 	}
 }
