5ea2d9e559d1307d9012e4f9cb5e9581f1d6a541
Gabor de Mooij <mail@gabordemooij.com>
2016-05-09 08:53:23 +0200

    Explain the purpose of the glue test.

--- testing/RedUNIT/Blackhole/Glue.php
+++ testing/RedUNIT/Blackhole/Glue.php
@@ -9,6 +9,20 @@ use RedBeanPHP\QueryWriter as QueryWriter;
 /**
  * Glue
  *
+ * RedBeanPHP does NOT parse entire queries and it does not
+ * ship with a query builder. However because RedBeanPHP
+ * facilitates mixing-in SQL snippets using methods like
+ * find(), via(), with(), withCondition() and so on...,
+ * it needs to be able to figure out how two query part strings
+ * can be connected to eachother. In particular parts beginning with or
+ * without 'WHERE', 'AND' and 'OR'. This test checks whether the
+ * QueryWriter has the ability to glue together query parts correctly.
+ * The gluer is part of the QueryWriter, however since this narrow
+ * slice of SQL syntax is so generic it's been implemented at Writer
+ * level and all drivers in RedBeanPHP inherit the generic implementation.
+ * At the moment of writing no additional glue methods had to be
+ * implemented.
+ *
  * @file    RedUNIT/Blackhole/Glue.php
  * @desc    Tests query gluer.
  * @author  Gabor de Mooij and the RedBeanPHP Community
@@ -29,21 +43,21 @@ class Glue extends Blackhole
 	{
 		$writer = R::getWriter();
 		//Can we add a condition without having to type 'WHERE' - usual suspects
-		asrt( $writer->glueSQLCondition(' name = ? '), ' WHERE  name = ? ' );
-		asrt( $writer->glueSQLCondition(' value1 > ? OR value < ? '), ' WHERE  value1 > ? OR value < ? ' );
+		asrt( $writer->glueSQLCondition( ' name = ? '), ' WHERE  name = ? ' );
+		asrt( $writer->glueSQLCondition( ' value1 > ? OR value < ? '), ' WHERE  value1 > ? OR value < ? ' );
 		//Does it recognize NON-WHERE conditions? - usual suspects
-		asrt( $writer->glueSQLCondition(' ORDER BY name '), ' ORDER BY name ' );
-		asrt( $writer->glueSQLCondition(' LIMIT 10 '), ' LIMIT 10 ' );
-		asrt( $writer->glueSQLCondition(' OFFSET 20 '), ' OFFSET 20 ' );
+		asrt( $writer->glueSQLCondition( ' ORDER BY name '), ' ORDER BY name ' );
+		asrt( $writer->glueSQLCondition( ' LIMIT 10 '), ' LIMIT 10 ' );
+		asrt( $writer->glueSQLCondition( ' OFFSET 20 '), ' OFFSET 20 ' );
 		//highly doubtful but who knows... - I think nobody will ever use this in a query snippet.
-		asrt( $writer->glueSQLCondition(' GROUP BY grp '), ' GROUP BY grp ' );
-		asrt( $writer->glueSQLCondition(' HAVING x = ? '), ' HAVING x = ? ' );
+		asrt( $writer->glueSQLCondition( ' GROUP BY grp '), ' GROUP BY grp ' );
+		asrt( $writer->glueSQLCondition( ' HAVING x = ? '), ' HAVING x = ? ' );
 		//can we replace WHERE with AND ?
-		asrt( $writer->glueSQLCondition(' AND name = ? ', QueryWriter::C_GLUE_WHERE ), ' WHERE  name = ? ' );
+		asrt( $writer->glueSQLCondition( ' AND name = ? ', QueryWriter::C_GLUE_WHERE ), ' WHERE  name = ? ' );
 		//can we glue with AND instead of WHERE ?
-		asrt( $writer->glueSQLCondition(' value1 > ? OR value < ? ', QueryWriter::C_GLUE_AND ), ' AND  value1 > ? OR value < ? ' );
+		asrt( $writer->glueSQLCondition( ' value1 > ? OR value < ? ', QueryWriter::C_GLUE_AND ), ' AND  value1 > ? OR value < ? ' );
 		//non-cases
-		asrt( $writer->glueSQLCondition(' GROUP BY grp ', QueryWriter::C_GLUE_WHERE ), ' GROUP BY grp ' );
-		asrt( $writer->glueSQLCondition(' GROUP BY grp ', QueryWriter::C_GLUE_AND ), ' GROUP BY grp ' );
+		asrt( $writer->glueSQLCondition( ' GROUP BY grp ', QueryWriter::C_GLUE_WHERE ), ' GROUP BY grp ' );
+		asrt( $writer->glueSQLCondition( ' GROUP BY grp ', QueryWriter::C_GLUE_AND ), ' GROUP BY grp ' );
 	}
 }
