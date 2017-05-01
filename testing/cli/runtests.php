42a47d0c76bd5904ccc445d0cddf6480888d4cd9
gabordemooij <gabor@redbeanphp.com>
2017-05-01 12:50:03 +0200

    Disable matchup tests

--- testing/cli/runtests.php
+++ testing/cli/runtests.php
@@ -120,7 +120,7 @@ $allPacks = array(
 	'Blackhole/Glue',
 	'Blackhole/Plugins',
 	'Blackhole/Debug',
-	'Base/Matchup',
+	//'Base/Matchup',
 	'Base/Quickexport',
 	'Base/Dispense',
 	'Base/Logging',
