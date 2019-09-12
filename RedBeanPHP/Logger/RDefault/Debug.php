9f63e522c5dd816f209d531def5b552119885407
Abygail Geurts <abygail@geurts.org>
2019-09-12 11:43:54 +0200

    Accept PDO Type bindings in Debug Logger
     This prevents logging throwing array to string conversion errors and displays the correct values.
    
    * Update the fillInValue to be able to parse the PDO::PARAM_INT and PDO::PARAM_STR options in bindings.
    * Add the $flagUseStringOnlyBinding, to match the Logger to the actual RedBead state.

--- RedBeanPHP/Logger/RDefault/Debug.php
+++ RedBeanPHP/Logger/RDefault/Debug.php
@@ -33,6 +33,11 @@ class Debug extends RDefault implements Logger
 	protected static $noCLI = FALSE;
 
 	/**
+	 * @var boolean
+	 */
+	protected $flagUseStringOnlyBinding = FALSE;
+
+	/**
 	 * Toggles CLI override. By default debugging functions will
 	 * output differently based on PHP_SAPI values. This function
 	 * allows you to override the PHP_SAPI setting. If you set
@@ -84,14 +89,28 @@ class Debug extends RDefault implements Logger
 	 */
 	protected function fillInValue( $value )
 	{
-		if ( is_null( $value ) ) $value = 'NULL';
+		if ( is_array( $value ) && count( $value ) == 2 ) {
+			$paramType = end( $value );
+			$value = reset( $value );
+		} else {
+			$paramType = NULL;
+		}
 
-		$value = strval( $value );
+		if ( is_null( $value ) ) if ( is_null( $value ) ) $value = 'NULL';
+
+		if ( $paramType != \PDO::PARAM_INT && $paramType != \PDO::PARAM_STR ) {
+			if ( !$this->flagUseStringOnlyBinding && \RedBeanPHP\QueryWriter\AQueryWriter::canBeTreatedAsInt( $value ) ) {
+				$paramType = \PDO::PARAM_INT;
+			} else {
+				$paramType = \PDO::PARAM_STR;
+			}
+		}
+
 		if ( strlen( $value ) > ( $this->strLen ) ) {
 			$value = substr( $value, 0, ( $this->strLen ) ).'... ';
 		}
 
-		if ( !\RedBeanPHP\QueryWriter\AQueryWriter::canBeTreatedAsInt( $value ) && $value !== 'NULL') {
+		if ($paramType === \PDO::PARAM_STR) {
 			$value = '\''.$value.'\'';
 		}
 
@@ -229,6 +248,21 @@ class Debug extends RDefault implements Logger
 	public function setParamStringLength( $len = 20 )
 	{
 		$this->strLen = max(0, $len);
+		return $this;
+	}
+
+	/**
+	 * Whether to bind all parameters as strings.
+	 * If set to TRUE this will cause all integers to be bound as STRINGS.
+	 * This will NOT affect NULL values.
+	 *
+	 * @param boolean $yesNo pass TRUE to bind all parameters as strings.
+	 *
+	 * @return self
+	 */
+	public function setUseStringOnlyBinding( $yesNo = false )
+	{
+		$this->flagUseStringOnlyBinding = (boolean) $yesNo;
 		return $this;
 	}
 }
