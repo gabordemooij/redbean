b3b5c0c6d0b6370ec4b717ec985252211a562576
Gabor de Mooij <gabordemooij@gmail.com>
2020-10-03 12:16:06 +0200

    Print PHP version in Unit Test.

--- testing/cli/runtests.php
+++ testing/cli/runtests.php
@@ -36,6 +36,9 @@ echo '*** RedUNIT ***'.PHP_EOL;
 echo 'Welcome to RedUNIT Unit testing framework for RedBeanPHP.'.PHP_EOL;
 echo PHP_EOL;
 
+echo sprintf('PHP version: %s', phpversion());
+echo PHP_EOL;
+
 /**
  * Define some globals.
  */
